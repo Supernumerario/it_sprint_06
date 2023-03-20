3e327ae95a9cc4c0dcb00ae71ae4529c71533fa3
Supernumerario <supernumerario@gmail.com>
2023-03-20 18:13:59 +0100

    Level 01 Exercise 03

--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,8 +1,20 @@
+import styled from 'styled-components'
+
+const TextLine = styled.div`
+  border: 2px solid black;
+  border-radius: 32px;
+  padding: 16px;
+  margin: 8px;
+  text-align: center;
+  font-size: 20px;
+  font-family: sans-serif;
+`;
+
 function Escena(props) {
 
   const textLines = props.text.map(line => {
     return (
-      <p key={line.id}>{line.text}</p>
+      <TextLine key={line.id}>{line.text}</TextLine>
     );
   });
 
